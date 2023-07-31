56f22ca031b91096045421c3bc1ea925b604ef85
Josue Isac Salomon Landa <josueisacsalomonlanda@gmail.com>
2023-07-30 23:53:44 -0600

    Creando repositorio

---
+++ proyecto con Josue/assets/js/controlador.js
@@ -0,0 +1,18 @@
+function accederTienda(){
+    document.getElementById('clientes1').style.display ="none";
+    document.getElementById('clientes2').style.display ="flex";
+}
+
+function accederLocales(){
+    document.getElementById('clientes1').style.display ="flex";
+    document.getElementById('clientes2').style.display ="none";
+}
+
+function mostrarEmpresa(){
+    console.log('probando')
+    document.getElementById('administradores').style.display ="flex";
+    document.getElementById('administradores_productos').style.display ="none";
+}
+
+
+
